221f16830d5477981ac0aa0dfbf0d0a2d70aa6cf
Dennis Adaku <dennisadaku@ymail.com>
2024-02-08 17:05:56 +0000

    added solution to task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,15 @@ def hash_password(password: str) -> bytes:
     pwd = password.encode()
     hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks the validity of a password
+
+        Args:
+            hashed_password (byte) : byte stringed hashed password
+            password (str): password to be checked
+
+        Returns (bool): boolean
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -47,8 +47,18 @@ import re
 
 
 # task 5
+# hash_password = __import__('encrypt_password').hash_password
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+# print(hash_password(password))
+
+
+# task 6
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+print(is_valid(encrypted_password, password))
