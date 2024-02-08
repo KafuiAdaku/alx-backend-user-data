1bf421b62717f02c10486b99af42730b8a027d71
Dennis Adaku <dennisadaku@ymail.com>
2024-02-08 14:16:54 +0000

    added solution to task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Logging module"""
+import logging
 from typing import List
 import re
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -22,3 +24,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(rf"{field}=.+?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in incoming log records using filter_datum
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
