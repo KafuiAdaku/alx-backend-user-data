2764cac9aa69e5a8afc886cba62014871386cf1f
Dennis Adaku <dennisadaku@ymail.com>
2024-02-23 02:03:50 +0000

    modified solution to tas 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,7 +51,7 @@ def logout():
     user = AUTH.get_user_from_session_id(session_id)
     if user is not None:
         AUTH.destroy_session(user.id)
-        return redirect(url_for("index"))
+        return redirect("/")
     else:
         abort(403)
 
