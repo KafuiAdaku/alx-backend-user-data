d30ababb38c93b13151b2cfc37d54555f064f5b9
Dennis Adaku <dennisadaku@ymail.com>
2024-02-23 00:33:14 +0000

    added solution to task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -13,19 +13,18 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route("/users", methods=["POST"], strict_slahses=False)
+@app.route("/users", methods=["POST"], strict_slashes=False)
 def users():
     """Users route"""
     email = request.form.get("email")
     password = request.form.get("password")
-    if not email and not password:
+    if email and password:
         try:
             new_user = AUTH.register_user(email, password)
             return jsonify({"email": email, "message": "user created"})
 
-        Except ValueError:
+        except ValueError:
             return jsonify({"message": "email already registered"}), 400
-
 
 
 if __name__ == "__main__":
