9cfe34a18f9febef30c4184bd81601b52acad5ea
Dennis Adaku <dennisadaku@ymail.com>
2024-02-23 01:08:36 +0000

    added solution to task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,13 @@ class Auth:
             return bcrypt.checkpw(encoded_pwd, hashed_pwd)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session ID"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
